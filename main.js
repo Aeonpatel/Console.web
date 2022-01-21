4ef0b5ed3830ebb7367e1ab934e8505d1b25ce96
Aeonpatel <85121945+Aeonpatel@users.noreply.github.com>
2022-01-21 20:48:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var number1=60;
+var number2=75;
+var last="Patel";
+var first="Aeon";
+var upper="hello".toUpperCase();
+var fullname="AeonPatel"
+
+console.log(first.concat(last))
+var added_numbers=number1+number2;
+console.log(added_numbers);
+console.log(upper);
+console.log(fullname.length)
